288c0847b478b1248b23df7ac93b1683ed5c0b50
Prince Owusu Afriyie <princeowusuafriyie20@gmail.com>
2026-05-10 23:32:50 +0000

    Day 7. For loops and range(). Multiplication tables. GH.

---
+++ day7.py
@@ -0,0 +1,5 @@
+number = int(input("What is your favorite number? "))
+for i in range(1,13):
+    result = number * i
+    print(f"{number} * {i} = {result}" )
+print("Study hard. Math is power. GH.")
