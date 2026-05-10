771e7c13add2d159a72a32643f4d678ee30708cc
Prince Owusu Afriyie <princeowusuafriyie20@gmail.com>
2026-05-10 02:14:51 +0000

    Day 1, Prince AI starts here. Kumasi,Ghana to the World

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py
+# Prince Owusu Afriyie . Prince AI 10
+# Day 1. May 9, 2026. Kumasi, Ghana
+# With God all things are possible.
+print("Hello Ghana")
+print("Prince AI starts here.")
+print("The walking started tonight.")
+print("Go demand a seat at the table.")
+
