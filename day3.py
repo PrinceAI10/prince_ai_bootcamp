0bc17089e8154cc28552919bb832c691467fce52
Prince Owusu Afriyie <princeowusuafriyie20@gmail.com>
2026-05-10 18:47:05 +0000

    Day 3.input() and int() conversion. Brain CPU listens now. GH

---
+++ day3.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+job = input("What is your dream job? ")
+year_of_birth = 2026-age
+print(f"Your name is {name} and you were born in {year_of_birth}. Your dream is to become an {job}" )
+
