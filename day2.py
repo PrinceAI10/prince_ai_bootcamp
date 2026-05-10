c5fa4024f60add7e0abe0ad4299835a8c4290323
Prince Owusu Afriyie <princeowusuafriyie20@gmail.com>
2026-05-10 17:38:49 +0000

    Day 2. Variables and f-strings. Brain CPU model in action. GH

---
+++ day2.py
@@ -0,0 +1,5 @@
+my_name = "Prince Owusu Afriyie"
+my_country = "Ghana"
+my_school = "Howard University"
+my_year = 2030
+print(f"My name is {my_name}. I am from {my_country}. I will graduate from {my_school} in {my_year}")
