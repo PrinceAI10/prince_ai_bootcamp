401e1ff069ae4033082b9182cb9bd21e6dd452fe
Prince Owusu Afriyie <princeowusuafriyie20@gmail.com>
2026-05-20 23:50:21 +0000

    Day 15. Q3. Exams merged score. GH.

---
+++ day15.py
@@ -0,0 +1,14 @@
+# TRIAL 3
+def merged_scores(dict1, dict2):
+    merged = {}
+    for student,score in dict1.items():
+    merged[student] = score
+    for student, score in dict2.items():
+        if score > score:
+            merged.append[score]
+        else:
+            merged.append[score]
+    return merged
+
+
+
